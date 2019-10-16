d8f76531538c9c67a1431f4be9615b102e55c01f
Samuel García <samuelalejandro0102@gmail.com>
2019-10-16 14:51:48 -0400

    1er commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { CitiesComponent } from './Home/cities/cities.component';
+import { ReservasComponent } from './admin/reservas/reservas.component';
+import { DestinosComponent } from './admin/destinos/destinos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    CitiesComponent,
+    ReservasComponent,
+    DestinosComponent
   ],
   imports: [
     BrowserModule,
