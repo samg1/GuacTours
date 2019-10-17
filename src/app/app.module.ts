aac4364cf1e4533cb3470c4920ffe8a4d70d6734
Juan Andres Lopez <juanlopezlmg@gmail.com>
2019-10-16 22:24:48 -0400

    Imagenes actualizadas, Componente Slider y NavBar creados

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { CitiesComponent } from './Home/cities/cities.component';
+import { ReservasComponent } from './admin/reservas/reservas.component';
+import { DestinosComponent } from './admin/destinos/destinos.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SliderComponent } from './slider/slider.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    CitiesComponent,
+    ReservasComponent,
+    DestinosComponent,
+    NavbarComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
